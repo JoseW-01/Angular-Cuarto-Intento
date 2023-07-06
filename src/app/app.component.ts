c968942a614a2215723fa98ac2400b2a335987cb
Jose William Martinez Pena <wilhouse180318@gmail.com>
2023-07-05 21:37:47 -0500

    se agrega crud simple de empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Cuarto-Intento';
+  employeeArray: Employee[];
+  selectedEmployee: Employee = new Employee();
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+
+    }
+
+    this.selectedEmployee = new Employee();
+  }
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+  delete() {
+    if (confirm("Are you sure want to delete it?")) {
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+
+  }
+  constructor() {
+    this.employeeArray = [
+      { id: 1, name: "Ryan", country: "USA" },
+      { id: 2, name: "Martha", country: "USA" },
+      { id: 3, name: "Joe", country: "USA" }
+    ];
+  }
+
 }
